586e699991a59ec29591a3f543ee148a019ea1a0
Salimgareev <kamilsalimgareev270@gmail.com>
2022-05-07 20:18:04 +0500

    создал кнопку назад

--- src/main/java/laba4/Calculator22.java
+++ src/main/java/laba4/Calculator22.java
@@ -2,6 +2,7 @@ package laba4;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,38 +11,42 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/Calculator22")
 public class Calculator22 extends HttpServlet {
-
-private static final long serialVersionUID = 1L;
-protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-     RequestCalc Calculator22 = RequestCalc.fromRequestParameters(request);
-	 Calculator22.setAsRequestAttributesAndCalculate(request);
-	 
-	 request.getRequestDispatcher("/Results.jsp").forward(request, response);
-  }
-   public static class RequestCalc{
-      private final String radius;
-	  private double result;
-   
-	  private RequestCalc(String radius) {
-		  this.radius = radius;
-	  }
-   
-	  public static RequestCalc fromRequestParameters(HttpServletRequest request){
-	   	return new RequestCalc(request.getParameter("radius"));
-	  }
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/Form.jsp");
+        dispatcher.forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		RequestCalc Calculator22 = RequestCalc.fromRequestParameters(request);
+		Calculator22.setAsRequestAttributesAndCalculate(request);
+		 
+		request.getRequestDispatcher("/Results.jsp").forward(request, response);
+	}
+	public static class RequestCalc{
+		private final String radius;
+		private double result;
 	   
-	  public void setAsRequestAttributesAndCalculate(HttpServletRequest request) {
-	      request.setAttribute("radius", radius);
-		  double radius_try;
-		  try{
-			  radius_try = Double.parseDouble(radius);
-		  }
-		  catch(NumberFormatException e){
-		     radius_try=0;
-		  }
-		  
-		  result= (4.0/3)*Math.PI*Math.pow(radius_try, 3.0);
-		  request.setAttribute("result", result);
-	  }
+		private RequestCalc(String radius) {
+			this.radius = radius;
+		}
+	   
+		public static RequestCalc fromRequestParameters(HttpServletRequest request){
+			return new RequestCalc(request.getParameter("radius"));
+		}
+		
+		public void setAsRequestAttributesAndCalculate(HttpServletRequest request) {
+			request.setAttribute("radius", radius);
+			double radius_try;
+			try{
+				radius_try = Double.parseDouble(radius);
+			}
+			catch(NumberFormatException e){
+				radius_try=0;
+			}
+			result= (4.0/3)*Math.PI*Math.pow(radius_try, 3.0);
+			request.setAttribute("result", result);
+		}
 	}
 }
--- src/main/webapp/Results.jsp
+++ src/main/webapp/Results.jsp
@@ -10,5 +10,8 @@
 <h2>Введённое значение радиуса:</h2>
    <p><strong>Радиус шара:</strong> ${radius}</p>
    <p><strong>Результат:</strong> ${result}</p>
- </body>
+   <form action="Form" method="get">
+		<div><input class="btnBack" name="btnBack" id = "btnBack" type="submit" value="Назад"></div>
+	</form>
+</body>
 </html>
